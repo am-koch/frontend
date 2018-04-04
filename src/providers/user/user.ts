34d32eb6499141bf47881ebf3451ac26c94efa57
smartin281092 <sebastien.martin@hook.sarl>
2018-04-04 18:17:22 +0200

    add POST

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -3,19 +3,21 @@ import { Injectable } from '@angular/core';
 
 import { User } from '../../models/user.model';
 
+import { ApiProvider } from '../api/api';
+
 @Injectable()
 export class UserProvider {
 
   public me: User;
 
   constructor(
-    public http: HttpClient
+    public api: ApiProvider
   ) {
-    console.log('Hello UserProvider');
   }
 
   setUser(data) {
     this.me = User.build(data);
+    this.api.post('users', data);
   } 
 
 }
